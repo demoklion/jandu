d5cc49524389e1139c515cd02552481241677707
jandu <mr.dugi@gmail.com>
2023-11-07 10:40:21 +0100

    minor formatting fixes

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
+* Start Bootstrap - Grayscale v4.1.1 (https://startbootstrap.com/template-overviews/grayscale)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
